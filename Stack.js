02e155b4da98621d2cd19e6371481dccee244a26
Chainn <578579786@qq.com>
2016-11-24 10:20:26 +0800

    add Stack

---
+++ Stack.js
@@ -0,0 +1,35 @@
+function Stack() {
+  var items = [];
+
+  this.push = function(element){
+    items.push(element);
+  };
+
+  this.pop = function(){
+    return items.pop();
+  };
+
+  this.peek = function(){
+    return items[items.length-1];
+  };
+
+  this.isEmpty = function(){
+    return items.length == 0;
+  };
+
+  this.size = function(){
+    return items.length;
+  };
+
+  this.clear = function(){
+    items = [];
+  };
+
+  this.print = function(){
+    console.log(items.toString());
+  };
+
+  this.get = function () {
+    return items;
+  }
+}
